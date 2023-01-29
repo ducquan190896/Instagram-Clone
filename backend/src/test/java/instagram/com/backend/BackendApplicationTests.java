90b35ac13f755d887cc868806b48fc2aa6689431
quan doan <ducquan190896@gmail.com>
2023-01-29 17:55:36 +0200

    init backend

---
+++ backend/src/test/java/instagram/com/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package instagram.com.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
